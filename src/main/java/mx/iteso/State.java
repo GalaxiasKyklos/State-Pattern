ea716b61b10ad5f74e4617185fa5395301e437f5
Rudycc <rcarcue@gmail.com>
2016-11-18 17:19:28 -0600

    cry no more

--- src/main/java/mx/iteso/State.java
+++ src/main/java/mx/iteso/State.java
@@ -4,5 +4,5 @@ package mx.iteso;
  * Created by Saúl on 17/11/2016.
  */
 public interface State {
-    String goNext(TelephonicLine telephonicLine, String context);
+    String goNext(TelephonicLine telephonicLine, int context);
 }
