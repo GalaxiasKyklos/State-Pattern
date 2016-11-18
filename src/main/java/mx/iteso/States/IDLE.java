58bba9482906371f8856b0cffdd85618b799d5e7
GalaxiasKyklos <saul_enrique-7285-96@hotmail.com>
2016-11-18 17:14:49 -0600

    Some Errors++

--- src/main/java/mx/iteso/States/IDLE.java
+++ src/main/java/mx/iteso/States/IDLE.java
@@ -7,9 +7,9 @@ import mx.iteso.TelephonicLine;
  * Created by Saúl on 17/11/2016.
  */
 public class IDLE implements State {
-    public String goNext(TelephonicLine telephonicLine, int context) {
+    public String goNext(TelephonicLine telephonicLine, TelephonicLine.contexts context) {
         switch (context) {
-            case TelephonicLine.contexts.AOff.ordinal():
+            case TelephonicLine.contexts.AOff:
                 break;
             case "Call entry":
                 break;
